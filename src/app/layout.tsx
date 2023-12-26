11c9f6d6206de8b2eefacb60619744a5ec5f5a53
mhasan-t <tahnoonn19@gmail.com>
2023-12-26 20:17:10 +0400

    seo moved to layout and loading state

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
+import Head from "next/head";
 import "./globals.css";
 import fonts from "@/app/fonts";
+
+const title = "Muhib Al Hasan | Software Engineer.";
+const description = "Portfolio of Muhib Al Hasan, Software Engineer.";
 
 export default function RootLayout({
 	children,
@@ -8,6 +12,42 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
+			<Head>
+				<meta
+					key="image"
+					name="og:image"
+					content="https://muhibalhasan.com/logo.png"
+				/>
+				<meta
+					key="description"
+					name="description"
+					content={description}
+				/>
+				<meta key="og-title" property="og:title" content={title} />
+				<meta
+					key="og-description"
+					property="og:description"
+					content={description}
+				/>
+				<meta
+					key="twitter-title"
+					name="twitter:title"
+					content={title}
+				/>
+				<meta
+					key="twitter-description"
+					name="twitter:description"
+					content={description}
+				/>
+				<meta name="twitter:card" content="summary_large_image" />
+				<meta name="twitter:site" content="@lighter_app" />
+				<meta
+					name="twitter:image"
+					content={`https://lighter.com/home-card.png`}
+				/>
+
+				<link rel="canonical" href="https://lighter.com/" />
+			</Head>
 			<body className={fonts.primary.className}>{children}</body>
 		</html>
 	);
