70629b3785d3cd7d9da7314187f81d3e9a87c42b
Muhib Al Hasan <37745982+mhasan-t@users.noreply.github.com>
2024-08-20 21:29:23 +0600

    Remove loading text

--- src/components/commons/LoadingComponent.tsx
+++ src/components/commons/LoadingComponent.tsx
@@ -7,7 +7,7 @@ function LoadingComponent({}: Props) {
 	return (
 		<div className="w-screen h-screen fixed z-50 bg-primary flex justify-center items-center">
 			<div className="bg-text_primary w-40 h-40 rounded-full flex justify-center items-center animate-beat">
-				<div className="text-black">loading</div>
+				{/* <div className="text-black">loading</div> */}
 			</div>
 		</div>
 	);
