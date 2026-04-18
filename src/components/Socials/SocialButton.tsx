b6e5d4b852368413abd172527d5ed6f2b09de25d
mhasan-t <muhibalhasan1@gmail.com>
2026-04-18 20:26:33 +0600

    removed hanging animation

--- src/components/Socials/SocialButton.tsx
+++ src/components/Socials/SocialButton.tsx
@@ -4,17 +4,15 @@ import { motion } from "framer-motion";
 type Props = { imgSrc: string; alt: string; link: string };
 
 export default function SocialButton({ imgSrc, alt, link }: Props) {
-	return (
-		<motion.a
-			layout
-			transition={{
-				duration: 4,
-				type: "keyframes",
-			}}
-			className="cursor-pointer"
-			href={link}
-		>
-			<motion.img src={imgSrc} alt={alt} className="w-8 h-8" />
-		</motion.a>
-	);
+  return (
+    <motion.a
+      href={link}
+      className="cursor-pointer"
+      whileHover={{ scale: 1.15 }}
+      whileTap={{ scale: 0.95 }}
+      transition={{ type: "spring", stiffness: 400, damping: 10 }}
+    >
+      <img src={imgSrc} alt={alt} className="w-8 h-8" />
+    </motion.a>
+  );
 }
--- src/components/Socials/index.tsx
+++ src/components/Socials/index.tsx
@@ -1,31 +1,12 @@
 import React, { memo } from "react";
-import { motion } from "framer-motion";
 import SocialButton from "./SocialButton";
 
 type Props = {};
 
-const hangRope = {
-  initial: {
-    height: "12vh",
-  },
-  animate: {
-    height: ["14vh", "12vh", "14vh"],
-    transition: {
-      repeat: Infinity,
-      duration: 1.4,
-      type: "easeInOut",
-    },
-  },
-};
-
 function Socials({ }: Props) {
   return (
     <div className="fixed right-4 top-12 z-30 hidden flex-col items-center gap-3 md:flex">
-      <motion.div
-        layout
-        {...hangRope}
-        className="h-[1px] w-10 bg-text_primary/30"
-      />
+      <div className="h-[1px] w-10 bg-text_primary/30" />
       <SocialButton
         imgSrc="/icons/gmail-soc.png"
         alt="email"
