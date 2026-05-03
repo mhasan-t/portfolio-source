6425c36c554f16a74e244bc2ae9d73026400261f
mhasan-t <muhibalhasan1@gmail.com>
2026-05-03 21:47:26 +0600

    Update fonts

--- src/app/fonts.tsx
+++ src/app/fonts.tsx
@@ -1,18 +1,18 @@
 import { Urbanist, Koulen, Heebo } from "next/font/google";
 
-const primary = Heebo({
-	weight: ["100", "300", "400", "500", "700", "900"],
-	subsets: ["latin"],
+  const primary = Urbanist({
+  weight: ["100", "300", "400", "500", "700", "900"],
+  subsets: ["latin"],
 });
 
-const title = Koulen({
-	weight: ["400"],
-	subsets: ["latin"],
+const title = Heebo({
+  weight: ["400"],
+  subsets: ["latin"],
 });
 
 const header = Urbanist({
-	weight: ["400", "500", "700"],
-	subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  subsets: ["latin"],
 });
 
 export default { primary, title, header };
