5900194045f02ff8586f90ffbc973c9dfd33af2d
mhasan-t <muhibalhasan1@gmail.com>
2026-05-04 12:50:36 +0600

    Update fonts.tsx

--- src/app/fonts.tsx
+++ src/app/fonts.tsx
@@ -1,20 +1,19 @@
-import { Urbanist, Koulen, Heebo } from "next/font/google";
+import { Urbanist, Koulen, Heebo, Inter, Black_Ops_One, Roboto, Quicksand } from "next/font/google";
 
-const primary = Urbanist({
-  weight: ["100", "300", "400", "500", "700", "900"],
+const primary = Quicksand({
+  weight: ["300", "400", "500", "700"],
   subsets: ["latin"],
 });
 
-const title = Heebo({
+const title = Black_Ops_One({
   weight: ["400"],
   subsets: ["latin"],
 });
 
-const header = Urbanist({
+const header = Quicksand({
   weight: ["400", "500", "700"],
   subsets: ["latin"],
 });
 
 const fonts = { primary, title, header };
-
 export default fonts;
